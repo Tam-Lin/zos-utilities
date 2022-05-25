ad46e32553271d9b80a6d046f71737a5eb9696d6
Kevin McKenzie <kmckenzi@us.ibm.com>
2022-05-25 09:56:52 -0400

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Tam-Lin/zos-utilities',
-    version='0.4.0',
+    version='0.5.0',
     zip_safe=False,
 )
